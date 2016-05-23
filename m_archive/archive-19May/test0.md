016dee0f290b66071afc79da6a69261bfd0f7e7c
nikak <nikakar@gmail.com>
2016-05-23 01:19:51 +0100

    updated r,conda,code

---
+++ m_archive/archive-19May/test0.md
@@ -0,0 +1,10 @@
+Title: My super title0
+Date: 2010-12-03 10:20
+Modified: 2010-12-05 19:30
+Category: Test
+Tags: pelican, publishing
+Slug: my-super-post0
+Authors: Conan Doyle
+Summary: Short version for index and feeds
+
+This is the content of my super blog post.
--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,11 @@ PATH = 'content'
 TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = u'en'
+DEFAULT_DATE = 'fs'
+DELETE_OUTPUT_DIRECTORY = False
+
+ARTICLE_URL = "articles/{date:%Y}{date:%m}{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "articles/{date:%Y}{date:%m}{date:%d}/{slug}/index.html"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -41,10 +46,16 @@ EXTRA_PATH_METADATA = {
     'extra/static/rStyle.css': {'path': 'static/rStyle.css'}
 }
 #from pelican.plugins import related_posts
-PLUGIN_PATHS = ['../pelican-plugins']
+PLUGIN_PATHS = ['../git_pelican-plugins/pelican-plugins']
 PLUGINS = ['gravatar','rmd_reader','series','liquid_tags','tag_cloud', 'tipue_search']
 #'assets', 'sitemap',
 
+#cash settings
+CACHE_CONTENT = True
+CACHE_PATH = 'cache'
+GZIP_CACHE = False
+CHECK_MODIFIED_METHOD = 'mtime'
+#WRITE_SELECTED = []
 
 # Specify theme
 THEME = "../pelican-themes/bootstrap3"
@@ -55,13 +66,28 @@ CUSTOM_CSS = 'static/rStyle.css'
 PYGMENTS_STYLE = 'solarizedlight'
 FAVICON = 'favicon.ico'
 SHOW_SERIES = True
-DISPLAY_SERIES_ON_SIDEBAR = False
+DISPLAY_SERIES_ON_SIDEBAR = True
 DISPLAY_CATEGORIES_ON_SIDEBAR  = True
+BOOTSTRAP_FLUID = False
 
 
 RMD_READER_CLEANUP = True
 #RMD_READER_KNITR_QUIET = True
 RMD_READER_KNITR_ENCODING = 'UTF-8'
-RMD_READER_KNITR_OPTS_CHUNK = None
+#RMD_READER_RENAME_PLOT='chunklabel'
+#RMD_READER_KNITR_OPTS_CHUNK = None
+#2-below duplicate folder structure of sourse files and 
+#name figures after sourse files which than are used in generated html
+#but also they create all these images in figures folder
+RMD_READER_RENAME_PLOT='directory'
+RMD_READER_KNITR_OPTS_CHUNK = {
+	'fig.path': 'figure/', 
+}
+
+
 
 MARKUP = ('md', 'ipynb', 'rmd')
+
+
+#How rmd_reader works is that it uses R to convert RMarkdown to Markdown
+#and then uses Python to convert that to HTML.
